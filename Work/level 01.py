bba8dcc6d6f2131b50a52ee33ef66f659252972f
Danny2Forever <105736002+Danny2Forever@users.noreply.github.com>
2022-08-23 19:34:29 +0700

    Create Level 1
    Initial comit

---
+++ Work/level 01.py
@@ -0,0 +1,16 @@
+#---------1--------- 
+print("\"Hello World!\"")
+
+#---------2--------- 
+b = int(input("birthday = "))
+print(f"I born in {b}.")
+
+#---------3--------- 
+a = input()
+b = int(input())
+print(a*b)
+
+#---------4--------- 
+weight = int(input("Enter weight : "))
+height = int(input("Enter height : "))
+print(f"{weight} {height}")
